3aeffa076ab0431022d0f8c75a78e99f356e980f
tomechava <techavarrg@eafit.edu.co>
2024-01-18 18:19:56 -0500

    estados sucursales

--- resources/views/sistema/estado_sucursal-nuevo.blade.php
+++ resources/views/sistema/estado_sucursal-nuevo.blade.php
@@ -12,11 +12,11 @@
 @section('breadcrumb')
 <ol class="breadcrumb">
       <li class="breadcrumb-item"><a href="/admin/home">Inicio</a></li>
-      <li class="breadcrumb-item"><a href="/admin/sucursales">Sucursales</a></li>
+      <li class="breadcrumb-item"><a href="/admin/sucursales/estados">Estados de Sucursales</a></li>
       <li class="breadcrumb-item active">Modificar</li>
 </ol>
 <ol class="toolbar">
-      <li class="btn-item"><a title="Nuevo" href="/admin/sucursal/nuevo" class="fa fa-plus-circle" aria-hidden="true"><span>Nuevo</span></a></li>
+      <li class="btn-item"><a title="Nuevo" href="/admin/sucursal/estado/nuevo" class="fa fa-plus-circle" aria-hidden="true"><span>Nuevo</span></a></li>
       <li class="btn-item"><a title="Guardar" href="#" class="fa fa-floppy-o" aria-hidden="true" onclick="javascript: $('#modalGuardar').modal('toggle');"><span>Guardar</span></a>
       </li>
       @if($globalId > 0)
@@ -26,14 +26,11 @@
 </ol>
 <script>
       function fsalir(){
-      location.href ="/admin/sucursales";
+      location.href ="/admin/sucursales/estados";
       }
 </script>
 @endsection
 @section('contenido')
-<?php
-use App\Entidades\Sistema\Estado;
-?>
 <div class="panel-body">
       <div id = "msg"></div>
       <?php
@@ -42,36 +39,13 @@ use App\Entidades\Sistema\Estado;
             }
       ?>
       <form id="form1" method="POST">
-      <div class="row">
+            <div class="row">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}"></input>
                 <input type="hidden" id="id" name="id" class="form-control" value="{{$globalId}}" required>
                 <div class="form-group col-lg-6">
                     <label>Nombre: *</label>
                     <input type="text" id="txtNombre" name="txtNombre" class="form-control" value="" required>
                 </div>
-                <div class="form-group col-lg-6">
-                    <label>Dirección: *</label>
-                    <input type="text" id="txtDireccion" name="txtDireccion" class="form-control" value="" required>
-                </div>
-            </div>
-            <div class="row">
-                <div class="form-group col-lg-6">
-                    <label>Telefono: *</label>
-                    <input type="text" id="txtTelefono" name="txtTelefono" class="form-control" value="" required>
-                </div>
-                <div class="form-group col-lg-6">
-                    <label>Mapa: *</label>
-                    <input type="text" id="txtMapa" name="txtMapa" class="form-control" value="" required>
-                </div>
-            </div>
-            <div class="row">
-            <div class="form-group col-lg-6">
-                    <label>Estado: *</label>
-                    <select name="lstEstadoSucursal" id="lstEstadoSucursal" class="form-control" required>
-                        <option value="" selected disabled>Seleccionar</option>
-                    </select>
-                </div>
-                
             </div>
       </form>
 </div>
