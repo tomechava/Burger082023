7a4b1ef705566a488bebafcc4c64e3e9688eeeb1
tomechava <techavarrg@eafit.edu.co>
2024-02-01 12:01:53 -0500

    nosotros comenzado

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ Route::group(array('domain' => '127.0.0.1'), function () {
 
     Route::get('/', 'ControladorWebHome@index');
     Route::get('/takeaway', 'ControladorWebTakeaway@index');
+    Route::get('/nosotros', 'ControladorWebNosotros@index');
+    
  
 
     Route::get('/admin', 'ControladorHome@index');
