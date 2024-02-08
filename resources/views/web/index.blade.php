f4e6b73e591660fcc70e3ac6e56d0e897b047697
tomechava <techavarrg@eafit.edu.co>
2024-02-08 14:06:09 -0500

    css

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -32,7 +32,7 @@
                 <div class="container">
                     <div class="row p-5">
                         <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
-                            <img class="img-fluid rounded shadow" src="web/images/triple.png" alt="">
+                            <img class="img-fluid rounded shadow" src="web/images/gallery-4.jpg" alt="">
                         </div>
                         <div class="col-lg-6 mb-0 d-flex align-items-center">
                             <div class="text-align-left">
